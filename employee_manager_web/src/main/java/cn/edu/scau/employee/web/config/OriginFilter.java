9e4bb09107b4b820497bea7b7d49f921ea20d867
chen <1367973836@qq.com>
2019-12-15 17:07:34 +0800

    解决跨域问题

---
+++ employee_manager_web/src/main/java/cn/edu/scau/employee/web/config/OriginFilter.java
@@ -0,0 +1,34 @@
+package cn.edu.scau.employee.web.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Collections;
+
+/**
+ * @author chen.jiale
+ * @Description 跨域请求过滤器
+ * @date 2019/12/15 16:09
+ */
+@Configuration
+public class OriginFilter {
+    @Bean
+    public FilterRegistrationBean corsFilter(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedOrigins(Collections.singletonList(CorsConfiguration.ALL));
+        corsConfiguration.setAllowedHeaders(Collections.singletonList(CorsConfiguration.ALL));
+        corsConfiguration.setAllowedMethods(Collections.singletonList(CorsConfiguration.ALL));
+        corsConfiguration.addExposedHeader("employee_token");
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
+}
